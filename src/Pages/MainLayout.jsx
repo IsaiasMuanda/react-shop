36a2ad3bc2e92a201f4f5273b164b2df113a0e3a
Isaias Marcos <143300660+IsaiasMuanda@users.noreply.github.com>
2025-04-13 22:35:23 +0100

    Corrige nome do arquivo Header.jsx com letra maiúscula

--- src/Pages/MainLayout.jsx
+++ src/Pages/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom'
 
-import Header from '../components/Header.jsx'
-import Footer from '../components/Footer.jsx'
+import Header from '../Components/Header.jsx'
+import Footer from '../Components/Footer.jsx'
 
 import React from 'react'
 
